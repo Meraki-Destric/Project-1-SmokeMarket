5f7bec38f77ecb624218e9664a90b5f429a7657b
Meraki-Destric <joshua.mu02@hotmail.com>
2021-10-28 19:56:36 +1100

    Initial commit

--- app/controllers/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class Registrations_Controller < Devise::Registrations_Controller
-  protected
-
-  def after_sign_up_path_for(resource)
-    # Set a path here and the user will be redirect there after they sign up
-    # Returns them to home
-    "/"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # The registrations controller will redirect the user after they signup
-  devise_for :users, controller: { registrations: "registrations" }
-  resources :library, only[:index]
-  resources :games do
-    # Create funtionality for the user to add and remove books from the library as they please
-    member do
-      put "add", to: "games#library"
-      put "remove", from: "games#library"
-    end
-  end
-
-  root "games#index"
-=======
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
->>>>>>> parent of 01cc2cd (Games Controller)
 end
