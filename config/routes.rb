5b7c9f8fac4dc992b1aed82cbdfc64e301ae8626
Meraki-Destric <joshua.mu02@hotmail.com>
2021-10-29 00:58:21 +1100

    LAst?

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Directs devise to use the registration controller
   devise_for :users, :controllers => { registrations: "registrations" }
   resources :posts
-  root "posts#index"
+  root "pages#index"
+  get "posts/index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
